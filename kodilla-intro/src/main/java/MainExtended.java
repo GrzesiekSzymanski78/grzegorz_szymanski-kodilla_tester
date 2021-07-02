eb77677ac9f70834c6971a056965e5777bf69e67
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-07-03 01:24:40 +0200

    Second Task - Declaring and calling variables

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+
+        String name = "Grzegorz";
+        String surname = "Szymański";
+        char firstLetterOfSurname = 'S';
+        int age = 43;
+        double height = 1.88;
+        boolean adult = true;
+
+        System.out.println(name);
+        System.out.println(surname);
+        System.out.println(firstLetterOfSurname);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(adult);
+    }
+}
