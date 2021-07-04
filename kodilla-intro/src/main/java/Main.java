876b6d6d56c49281ae305c6402a2fb1b0fe2773f
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-07-04 15:42:51 +0200

    Second Task - Declaring and calling variables

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Grzesiek";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        String text = "Hello from FirstClass";
+        System.out.println(myName);
+        System.out.println(text);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+    }
+}
