d936942e783085370c0bbcfe92d1d669ef63c57c
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-08-03 22:21:42 +0200

    Task - Collections - Stamps

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
@@ -0,0 +1,20 @@
+package com.kodilla.collections.sets.homework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StampsApplication {
+    public static void main(String[] args) {
+        Set<Stamp> stamps = new HashSet<>();
+        stamps.add(new Stamp("Flower", 15.5, true));
+        stamps.add(new Stamp("Diamond", 10.2, true));
+        stamps.add(new Stamp("Flower", 15.5, true));
+        stamps.add(new Stamp("Sky", 12.3, false));
+        stamps.add(new Stamp("Diamond", 10.2, true));
+        stamps.add(new Stamp("Sky", 12.3, false));
+
+        System.out.println(stamps.size());
+        for (Stamp stamp : stamps)
+            System.out.println(stamp);
+    }
+}
