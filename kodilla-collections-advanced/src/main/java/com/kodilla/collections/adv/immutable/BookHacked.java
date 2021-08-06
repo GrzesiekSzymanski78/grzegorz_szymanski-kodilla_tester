3d9e603786bb42d3ad27eadc090f5f98f4a6c91e
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-08-06 21:47:23 +0200

    Exercise - Immutable class Book

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/BookHacked.java
@@ -0,0 +1,9 @@
+package com.kodilla.collections.adv.immutable;
+
+public class BookHacked {
+    private String realTitle;
+
+    public void modifyTitle(String newTitle) {
+        realTitle = newTitle;
+    }
+}
