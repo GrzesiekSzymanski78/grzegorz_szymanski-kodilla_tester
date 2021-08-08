826163d2f942405c4f63ec098c7b5c7e10077825
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-08-08 22:34:01 +0200

    Task - Class Task - Immutable - fix

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskApplication.java
@@ -4,7 +4,6 @@ public class TaskApplication {
     public static void main(String[] args) {
 
         Task task = new Task("programming", 30);
-//        Task task1 = new TaskHacking("sleeping",40);
         System.out.println("Name of task: " + task.getTitle() + ", Time in minutes: " + task.getDuration());
     }
 }
