b23799af64117094f6fd2c565378cdc601a367bb
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-08-09 21:29:03 +0200

    Task BookManager - fix

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -12,6 +12,12 @@ public class BookApplication {
 
         Book b1 = new Book("Punishment and Crime", "Fyodor Dostoevsky");
         Book b2 = new Book("Punishment and Crime", "Fyodor Dostoevsky");
+        Book b3 = new Book("The girl with the dragoon tattoo","Stieg Larsson");
+        Book b4 = new Book("The girl with the dragoon tattoo","Stieg Larsson");
+        Book b5 = new Book("The Snowman", "Jo Nesbo");
+        Book b6 = new Book("The Snowman", "Jo Nesbo");
         System.out.println("Compare addresses: " + (b1 == b2));
+        System.out.println("b3.equals(b4): " + b3.equals(b4));
+        System.out.println("b5.equals(b6): " + b5.equals(b6));
     }
 }
