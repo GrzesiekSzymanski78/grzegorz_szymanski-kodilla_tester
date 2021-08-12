1f64064134a77bb10f08282af4f47a42d1bffa34
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-08-12 22:35:02 +0200

    Task - SchoolDirectory - change of variable names

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -7,17 +7,17 @@ public class SchoolDirectory {
     public static void main(String[] args) {
         Map<School, Principal> schoolPrincipalHashMap = new HashMap<>();
 
-        School sSch = new School("Secondary School Chicago", 20,25,22,23);
-        School sSd = new School("Secondary School Detroit", 30,25,28,24);
-        School sSn = new School("Secondary School New York", 24,29,23,26);
+        School firstSchool = new School("Secondary School Chicago", 20,25,22,23);
+        School secondSchool = new School("Secondary School Detroit", 30,25,28,24);
+        School thirdSchool = new School("Secondary School New York", 24,29,23,26);
 
-        Principal sSchPrincipal = new Principal("John", "Travolta");
-        Principal sSdPrincipal = new Principal("Uma", "Thurman");
-        Principal sSPrincipal = new Principal("Kevin", "Costner");
+        Principal firstSchoolPrincipal = new Principal("John", "Travolta");
+        Principal secondSchoolPrincipal = new Principal("Uma", "Thurman");
+        Principal thirdSchoolPrincipal = new Principal("Kevin", "Costner");
 
-        schoolPrincipalHashMap.put(sSch, sSchPrincipal);
-        schoolPrincipalHashMap.put(sSd, sSdPrincipal);
-        schoolPrincipalHashMap.put(sSn, sSPrincipal);
+        schoolPrincipalHashMap.put(firstSchool, firstSchoolPrincipal);
+        schoolPrincipalHashMap.put(secondSchool, secondSchoolPrincipal);
+        schoolPrincipalHashMap.put(thirdSchool, thirdSchoolPrincipal);
 
         for (Map.Entry<School, Principal> schoolPrincipalEntry : schoolPrincipalHashMap.entrySet())
             System.out.println("School director: " + schoolPrincipalEntry.getValue().getFirstName() + " " +
