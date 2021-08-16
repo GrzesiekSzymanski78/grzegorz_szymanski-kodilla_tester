cdb2d20f0eca7cbe8dde35b7c32ee6c9aee86838
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-08-16 16:51:53 +0200

    Task - FlightRepositoryTest - fix

--- kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homwork/FlightRepositoryTest.java
+++ kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homwork/FlightRepositoryTest.java
@@ -16,6 +16,10 @@ public class FlightRepositoryTest {
     @Test
     public void testFindFlightsTo() {
         List<Flight> result = flightFinder.findFlightTo("Paris");
+        result.add(new Flight("Warsaw","Paris"));
+        result.add(new Flight("Berlin","Paris"));
+        result.add(new Flight("Barcelona","Paris"));
+
         List<Flight> expectedList = new ArrayList<>();
         expectedList.add(new Flight("Warsaw", "Paris"));
         expectedList.add(new Flight("Berlin", "Paris"));
@@ -26,9 +30,14 @@ public class FlightRepositoryTest {
     @Test
     public void testFindFlightsFrom() {
         List<Flight> result = flightFinder.findFlightsFrom("Prague");
+        result.add(new Flight("Prague","Warsaw"));
+        result.add(new Flight("Prague","Berlin"));
+        result.add(new Flight("Prague","London"));
+        
         List<Flight> expectedList = new ArrayList<>();
         expectedList.add(new Flight("Prague", "Warsaw"));
         expectedList.add(new Flight("Prague", "Berlin"));
+        expectedList.add(new Flight("Prague","London"));
         assertEquals(expectedList, result);
     }
 }
