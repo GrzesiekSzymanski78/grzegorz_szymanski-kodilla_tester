1462d14ce678e8344427e61ae2aaec0fc55e86c6
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-08-18 23:19:54 +0200

    Task - OrderDoesntExistException

---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WareHouseApp.java
@@ -0,0 +1,27 @@
+package com.kodilla.exception.homework;
+
+import java.util.Scanner;
+
+public class WareHouseApp {
+
+    public static void main(String[] args) {
+
+        WareHouse wareHouse = new WareHouse();
+
+        wareHouse.addOrder(new Order("1"));
+        wareHouse.addOrder(new Order("2"));
+        wareHouse.addOrder(new Order("3"));
+        wareHouse.addOrder(new Order("4"));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your order number(from 1 to 4): ");
+        String orderNumber = scanner.nextLine();
+
+        try {
+            wareHouse.getOrder(orderNumber);
+            System.out.println("Your order is in the warehouse.");
+        } catch (OrderDoesntExistException e) {
+            System.out.println("Sorry we don't have this number of order.");
+        }
+    }
+}
