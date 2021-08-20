cba1744420741bd0343c0f65ecc6a8099f5cbc52
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-08-20 22:24:24 +0200

    Exercises - StringManipulatorTestSuite with CSV file

--- src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.parametrized_tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,13 +11,13 @@ class StringManipulatorTestSuite {
     private StringManipulator manipulator = new StringManipulator();
 
     @ParameterizedTest
-    @CsvSource(value = {"test,tset", "OtHer,rehto", "EVent,tneve", "null,llun", "A,a"})
+    @CsvFileSource(resources = "/stringWithLowerCase.csv",numLinesToSkip = 1)
     public void shouldReverseStringWithLowerCase(String input, String expected) {
         assertEquals(expected, manipulator.reverseWithLowerCase(input));
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"test,4", " OtHer, 5", "E v e n t,5", "null,4", "A,1"})
+    @CsvFileSource(resources = "/stringLengthWithoutSpaces.csv",numLinesToSkip = 1)
     public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
         assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
     }
