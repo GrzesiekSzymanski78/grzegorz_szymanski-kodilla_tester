d0765c74e4a0684ab37d5959d81b4c42c5fee07e
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-09-01 00:54:45 +0200

    Task - ShopTestSuite

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
@@ -0,0 +1,39 @@
+package com.kodilla.execution_model.homework;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Shop {
+
+    public List<Order> shopOrders = new ArrayList<>();
+
+    public void addOrder(Order order) {
+        this.shopOrders.add(order);
+    }
+
+    public List<Order> getOrdersInTwoDatesRange(LocalDate startDate, LocalDate endDate) {
+        return shopOrders.stream()
+                .filter(d -> d.getOrderDate().isAfter(startDate) && d.getOrderDate().isBefore(endDate)
+                        || d.getOrderDate().isEqual(startDate) || d.getOrderDate().isEqual(endDate))
+                .collect(Collectors.toList());
+    }
+
+    public List<Order> getOrdersInValueRange(double min, double max) {
+        return shopOrders.stream()
+                .filter(v -> v.getOrderValue() >= min && v.getOrderValue() <= max)
+                .collect(Collectors.toList());
+    }
+
+    public int getOrderSize() {
+        return shopOrders.size();
+    }
+
+    public Order getOrder(int index) {
+        if (index >= 0 && index < shopOrders.size()) {
+            return this.shopOrders.get(index);
+        }
+        return null;
+    }
+}
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/ShopTestSuite.java
@@ -0,0 +1,106 @@
+package com.kodilla.execution_model.homework;
+
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopTestSuite {
+
+    Shop shop = new Shop();
+    Order order1 = new Order(125.99, LocalDate.of(2021, 06, 01), "Kate81");
+    Order order2 = new Order(60.00, LocalDate.of(2021, 04, 30), "Johnny25");
+    Order order3 = new Order(55.50, LocalDate.of(2021, 05, 29), "Dave02");
+    Order order4 = new Order(260.00, LocalDate.of(2021, 04, 15), "JackBlack");
+    Order order5 = new Order(32.20, LocalDate.of(2021, 06, 23), "Harry02");
+
+    @Test
+    public void shouldAddOrdersToOrderList() {
+        int numberOfOrders = shop.shopOrders.size();
+        assertEquals(5, numberOfOrders);
+    }
+
+    @Test
+    public void shouldGetOrdersInDatesRange() {
+        List<Order> result = shop.getOrdersInTwoDatesRange(LocalDate.of(2021, 04, 15),
+                LocalDate.of(2021, 04, 30));
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    public void shouldGetOrdersInValueRange() {
+        List<Order> orders = shop.getOrdersInValueRange(30, 60);
+        assertEquals(3, orders.size());
+    }
+
+    @Test
+    public void shouldReturnEmptyListIfDatesOutOfRange() {
+        List<Order> emptyList = shop.getOrdersInTwoDatesRange(LocalDate.of(2021, 01, 01),
+                LocalDate.of(2021, 03, 31));
+        assertEquals(Collections.emptyList(), emptyList);
+    }
+
+    @Test
+    public void shouldReturnEmptyListIfValuesOutOfRange() {
+        List<Order> emptyList = shop.getOrdersInValueRange(10, 30);
+        assertEquals(Collections.emptyList(), emptyList);
+    }
+
+    @Test
+    public void shouldSelectedOrderExist() {
+        Order result = shop.shopOrders.get(0);
+        assertEquals("Kate81", result.getUserLogin());
+        assertEquals(125.99, result.getOrderValue());
+        assertEquals(LocalDate.of(2021, 06, 01), result.getOrderDate());
+    }
+
+    @Test
+    public void shouldExistingIndexOfOrder() {
+        Order result = shop.getOrder(3);
+        assertEquals(shop.getOrder(3), result);
+    }
+
+    @Test
+    public void shouldReturnNullWhenPassingOutOfRangeIndex() {
+        Order result = shop.getOrder(5);
+        assertNull(result);
+    }
+
+    @Test
+    public void shouldReturnNullWhenPassingNegativeIndex() {
+        Order result = shop.getOrder(-1);
+        assertNull(result);
+    }
+
+    @Test
+    public void shouldClearExistingOrders() {
+        shop.shopOrders.clear();
+        assertEquals(0, shop.getOrderSize());
+    }
+
+    @BeforeEach
+    public void initializeOrders() {
+        shop.addOrder(order1);
+        shop.addOrder(order2);
+        shop.addOrder(order3);
+        shop.addOrder(order4);
+        shop.addOrder(order5);
+    }
+    @AfterEach
+    public void resetValues(){
+        System.out.println("Resetting values...");
+    }
+
+    @BeforeAll
+    public static void displayIntroMessages(){
+        System.out.println("Starting testing");
+    }
+
+    @AfterAll
+    public static void displayGoodbyeMessage(){
+        System.out.println("Finishing testing");
+    }
+}
