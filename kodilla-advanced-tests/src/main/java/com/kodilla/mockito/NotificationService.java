99f37d08aad40011f0c1f202fb1bc37ecb2c6ec3
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-09-03 22:02:39 +0200

    Exercises - NotificationService - removing non active imports

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/NotificationService.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/NotificationService.java
@@ -1,10 +1,6 @@
 package com.kodilla.mockito;
 
-import org.w3c.dom.stylesheets.LinkStyle;
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class NotificationService {
