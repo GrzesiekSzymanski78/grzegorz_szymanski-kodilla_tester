2ea5c36c3a818127ad8adb3ebc5bd11c6f252044
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-10-01 23:58:55 +0200

    Exercises - 13.3 First test Google

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -0,0 +1,20 @@
+package com.kodilla.selenium.google;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleExample {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Selenium-drivers\\Chrome\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        WebElement imputField = driver.findElement(By.name("q"));
+        WebElement consentToCookiesBtn = driver.findElement(By.id("L2AGLb"));
+        consentToCookiesBtn.click();
+        imputField.sendKeys("Kodilla");
+        imputField.submit();
+    }
+}
