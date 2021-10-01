ccd214f8a35c87e8375b65306e3260407b9bf5d3
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-10-02 00:01:04 +0200

    Task - eBay

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -0,0 +1,22 @@
+package com.kodilla.selenium.ebay;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EBayTestingApp {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium-drivers\\Chrome\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com/");
+        driver.manage().window().maximize();
+
+        WebElement imputField = driver.findElement(By.id("gh-ac"));
+        imputField.sendKeys("Laptop");
+        imputField.submit();
+
+        driver.close();
+    }
+}
