b44827d0b4eb27d3f4e7b177085115f8e12d4a3e
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-10-10 12:49:58 +0200

    Task - css selector exercises

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -5,22 +5,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class AllegroTestingApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RuntimeException, InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl/");
         driver.manage().window().maximize();
 
-        WebElement acceptBtn = driver.findElement(By.xpath("//*[@data-role='accept-consent']"));
+        WebElement acceptBtn = driver.findElement(By.cssSelector("button[data-role=accept-consent"));
         acceptBtn.click();
-        WebElement categoryElectronic = driver.findElement(By.xpath("//*[@label='Kategorie']//child::option[3]"));
+        WebElement categoryElectronic = driver.findElement(By.linkText("Elektronika"));
         categoryElectronic.click();
-        WebElement inputField = driver.findElement(By.xpath("(//*[@data-role='search-form']//following::input)[1]"));
+        WebElement inputField = driver.findElement(By.cssSelector("input[data-role=search-input]"));
         inputField.sendKeys("Mavic mini");
         inputField.submit();
-
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        List<WebElement> elements = driver.findElements(By.cssSelector("[data-item=true]"));
+        System.out.println(elements.size());
+        for (int i = 0; i < elements.size(); i++) {
+            System.out.println(elements.get(i).getText());
+        }
         driver.quit();
     }
 }
