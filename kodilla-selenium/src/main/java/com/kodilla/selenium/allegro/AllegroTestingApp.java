d43550e2ccb12abc07e7b0936f4f3e74f1f949bc
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-10-10 10:54:57 +0200

    Task - 13.4 xpath exercises

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AllegroTestingApp {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Selenium-drivers\\Chrome\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl/");
         driver.manage().window().maximize();
