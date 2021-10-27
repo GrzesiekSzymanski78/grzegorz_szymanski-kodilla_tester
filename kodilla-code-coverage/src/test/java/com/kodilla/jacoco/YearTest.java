36a2d3b0b393420c44f6203d0a0ead061e4e7385
Grzegorz Szymański <grzesieksz78@gmail.com>
2021-10-27 19:55:05 +0200

    15.2 - Code analysis tools

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -0,0 +1,61 @@
+package com.kodilla.jacoco;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class YearTest {
+
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy400() {
+
+        //given
+        Year year = new Year(1600);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy4() {
+
+        //given
+        Year year = new Year(1988);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfDivisibleBy100ButNotDivisibleBy400() {
+
+        //given
+        Year year = new Year(1500);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfNotDivisibleBy4() {
+
+        //given
+        Year year = new Year(1993);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertFalse(isLeapYear);
+    }
+}
